c5b682a96bcc325ab147c57349cafbb25268efd7
NicholasWessinger <nicholaswessingerwork@gmail.com>
2021-07-01 16:41:12 -0400

    Added a HighScore and a Reset HighScore button.

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -6,27 +6,41 @@ using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
-
     public Text MyscoreText;
-    private int ScoreNum;
+    public Text HighScoreText;
+    int ScoreNum = 0;
+    int Highscore = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        ScoreNum = 0;
         MyscoreText.text = "Score : " + ScoreNum;
 
+        Highscore = PlayerPrefs.GetInt("highscore", 0);
+        HighScoreText.text = "HIGHSCORE: " + Highscore.ToString();
 
 
     }
+
+    public void ResetHS()
+    {
+        PlayerPrefs.SetInt("highscore", 0);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D Coin)
     {
         if (Coin.tag == "MyCoin")
         {
             ScoreNum += 1;
-            Destroy(Coin.gameObject); // delete this for it to stay once you hit it. //
             MyscoreText.text = "Score : " + ScoreNum;
+            if (Highscore < ScoreNum)
+            {
+                PlayerPrefs.SetInt("highscore", ScoreNum);
+            }
+            
+            
         }
 
     }
