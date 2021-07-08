1e41a8f58d38b2334918cd75d3fbe9e6fc74d9cc
NicholasWessinger <nicholaswessingerwork@gmail.com>
2021-07-08 09:57:28 -0400

    Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        if ((t % 60) > 10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -4);
+        }
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
